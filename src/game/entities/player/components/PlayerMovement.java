ec702ad00db87f59a3cb78659f8c426d28be0372
BrianAndres <shadownumberz@gmail.com>
2021-02-10 13:08:28 -0500

    Update PlayerMovement.java

--- src/game/entities/player/components/PlayerMovement.java
+++ src/game/entities/player/components/PlayerMovement.java
@@ -7,15 +7,20 @@ import java.awt.event.KeyEvent;
 import core.KeyInput;
 
 public class PlayerMovement extends Component {
+	
 	private int stamina = 100;
+	double defaultvelocity = 5;
+	double currentvelocity = defaultvelocity;
+	
 	public void update() {
 		
 		double y = entity.getY();
 		double x = entity.getX();
 		
-		double defaultvelocity = 5;
-		double currentvelocity = defaultvelocity;
 		
+		
+		
+		//shift to sprint, uses stamina up
 		if (KeyInput.get(KeyEvent.VK_SHIFT)) {
 			if(stamina != 0) {
 				stamina-= 1;
@@ -24,26 +29,32 @@ public class PlayerMovement extends Component {
 			if (stamina > 0)
 				currentvelocity = defaultvelocity+3;
 	    }
+		//if not pressing shift, stamina regens and resets current velocity
 		else {
 			if (stamina != 100)
 				stamina += 1;
-			System.out.println("else works!");
+			currentvelocity = defaultvelocity;
 		}
+		
+		//move left
 	    if (KeyInput.get(KeyEvent.VK_A)) {
 		   x-=currentvelocity;
 		   entity.setX(x);
 		 }
-
+	    
+	    //move right
 		 if (KeyInput.get(KeyEvent.VK_D)) {
 		    x+=currentvelocity;
 		    entity.setX(x);
 		 }
-
+		 
+		 //move up
 		 if (KeyInput.get(KeyEvent.VK_W)) {
 		    y-=currentvelocity;
 		    entity.setY(y);
 		 }
-
+		 
+		 //move down
 		 if (KeyInput.get(KeyEvent.VK_S)) {
 		    y+=currentvelocity;
 		    entity.setY(y);
