ee040fc13fc83c0d668b654e4d68c4a661544828
BrianAndres <shadownumberz@gmail.com>
2021-02-10 13:01:55 -0500

    added sprint (shift) and stamina
    added the ability to temporary sprint by pushing shift but uses stamina that refreshes naturally as time goes on.

--- src/game/entities/player/components/PlayerMovement.java
+++ src/game/entities/player/components/PlayerMovement.java
@@ -7,33 +7,48 @@ import java.awt.event.KeyEvent;
 import core.KeyInput;
 
 public class PlayerMovement extends Component {
-	
+	private int stamina = 100;
 	public void update() {
 		
 		double y = entity.getY();
 		double x = entity.getX();
 		
+		double defaultvelocity = 5;
+		double currentvelocity = defaultvelocity;
+		
+		if (KeyInput.get(KeyEvent.VK_SHIFT)) {
+			if(stamina != 0) {
+				stamina-= 1;
+				System.out.println(stamina);
+			}
+			if (stamina > 0)
+				currentvelocity = defaultvelocity+3;
+	    }
+		else {
+			if (stamina != 100)
+				stamina += 1;
+			System.out.println("else works!");
+		}
+	    if (KeyInput.get(KeyEvent.VK_A)) {
+		   x-=currentvelocity;
+		   entity.setX(x);
+		 }
+
+		 if (KeyInput.get(KeyEvent.VK_D)) {
+		    x+=currentvelocity;
+		    entity.setX(x);
+		 }
+
+		 if (KeyInput.get(KeyEvent.VK_W)) {
+		    y-=currentvelocity;
+		    entity.setY(y);
+		 }
+
+		 if (KeyInput.get(KeyEvent.VK_S)) {
+		    y+=currentvelocity;
+		    entity.setY(y);
+		 }
 		    
-		    if (KeyInput.get(KeyEvent.VK_A)) {
-		    	x-=5;
-		    	entity.setX(x);
-		 
-		    }
-
-		    if (KeyInput.get(KeyEvent.VK_D)) {
-		        x+=5;
-		    	entity.setX(x);
-		    }
-
-		    if (KeyInput.get(KeyEvent.VK_W)) {
-		    	y-=5;
-		    	entity.setY(y);
-		    }
-
-		    if (KeyInput.get(KeyEvent.VK_S)) {
-		    	y+=5;
-		    	entity.setY(y);
-		    }
 		    
 		
 		
