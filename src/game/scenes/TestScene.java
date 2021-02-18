f8c28a0b1e0704dd04b59cd609fa38c39bb7107e
BrianAndres <shadownumberz@gmail.com>
2021-02-18 15:04:47 -0500

    idk what this is just trying to update

--- src/game/scenes/TestScene.java
+++ src/game/scenes/TestScene.java
@@ -8,13 +8,13 @@ public class TestScene extends Scene {
 	public void init() {
 	
 		Player player = new Player(30, 30, this);
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		
 	}
-=======
-	
- 	}
->>>>>>> 594b1e968d2a71613751e344bac3cc38a2ed38f4
+//=======
+//	
+// 	}
+//>>>>>>> 594b1e968d2a71613751e344bac3cc38a2ed38f4
 	public void action() {
 		
 		
