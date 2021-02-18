1ab0b6cebb12fdb2e9b14cdf3415d1bda9f1ff87
Funnyblue1 <71399145+Funnyblue1@users.noreply.github.com>
2021-02-18 15:41:43 -0500

    EnemyAI created

---
+++ src/game/entities/enemies/testenemy/components/EnemyAI.java
@@ -0,0 +1,60 @@
+package game.entities.enemies.testenemy.components;
+
+import core.components.Component;
+
+public class EnemyAI extends Component {
+	
+	private int x, y;
+	
+	public EnemyAI(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void update() {
+		x = (int)-entity.getX();
+		y = (int)-entity.getY();
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void detect() {
+		private Player player = (Player) entity.getScene().getEntity("Player");
+		private Enemy enemy = (Enemy) entity.getScene().getEntity("Enemy");
+		
+		//move left
+		if(player.getX() < enemy.getX()) {
+			x -= 1;
+		}
+		
+		//move right
+		if(player.getX() > enemy.getX()) {
+			x += 1;
+		}
+		
+		//move down
+		if(player.getY() < enemy.getY()) {
+			y -= 1;
+		}
+		
+		//move left
+		if(player.getX() > enemy.getX()) {
+			y += 1;
+		}
+				
+	}
+}
