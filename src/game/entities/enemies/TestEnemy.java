83aad22a9ba9e5c30eea19c37f5907600de34569
vpilone <78926441+vpilone@users.noreply.github.com>
2021-03-11 15:42:07 -0500

    ???

--- src/game/entities/enemies/TestEnemy.java
+++ src/game/entities/enemies/TestEnemy.java
@@ -1,8 +1,9 @@
 package game.entities.enemies;
 
 import core.Scene;
+import core.entities.Entity;
 
-public class TestEnemy extends Enemy{
+public class TestEnemy extends Entity{
 
 	public TestEnemy(int x, int y, Scene scene) {
 		super(x, y, scene);
